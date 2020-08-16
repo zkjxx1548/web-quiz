f9bc6af2cfab044eed251558f57c30e41f2e55ca
zkj <438101845@qq.com>
2020-08-16 22:15:23 +0800

    add api (/product/id)

--- src/main/java/com/thoughtworks/webquiz/controller/ProductController.java
+++ src/main/java/com/thoughtworks/webquiz/controller/ProductController.java
@@ -2,29 +2,32 @@ package com.thoughtworks.webquiz.controller;
 
 
 import com.thoughtworks.webquiz.domain.Product;
+import com.thoughtworks.webquiz.dto.OrderDto;
 import com.thoughtworks.webquiz.dto.ProductDto;
+import com.thoughtworks.webquiz.repository.OrderRepository;
 import com.thoughtworks.webquiz.repository.ProductRepository;
+import javafx.scene.canvas.GraphicsContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
     @Autowired
     ProductRepository productRepository;
+    @Autowired
+    OrderRepository orderRepository;
 
 
-    public static void init() {
-
-    }
+//    public static void init() {
+//
+//        new ProductRepository().save(ProductDto.builder().name("可乐1").price(1).unit("瓶").build());
+//    }
 
     @GetMapping("/product")
     public ResponseEntity<List<Product>> getProductList() {
@@ -44,6 +47,16 @@ public class ProductController {
     @GetMapping("/product/{id}")
     public ResponseEntity<Product> addProductToOrder(@PathVariable int id) {
         ProductDto productDto = productRepository.findById(id).get();
+
+        OrderDto orderDto = OrderDto.builder()
+                .name(productDto.getName())
+                .productId(id)
+                .price(productDto.getPrice())
+                .unit(productDto.getUnit())
+                .count(1)
+                .build();
+
+        orderRepository.save(orderDto);
 
         Product product = Product.builder()
                 .name(productDto.getName())
--- src/main/java/com/thoughtworks/webquiz/dto/OrderDto.java
+++ src/main/java/com/thoughtworks/webquiz/dto/OrderDto.java
@@ -1,14 +1,14 @@
 package com.thoughtworks.webquiz.dto;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.persistence.Table;
+
 
 @Entity
 @Data
