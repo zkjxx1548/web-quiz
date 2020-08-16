02376595de568117c57dc35368c5d601bdb0a18a
zkj <438101845@qq.com>
2020-08-16 16:40:08 +0800

    init

---
+++ src/main/java/com/thoughtworks/webquiz/WebQuizApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.webquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtworks/webquiz/WebQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.webquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
